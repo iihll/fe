c6938747798eaf72aa94e5e70af70b0853290f37
iihll <iihll.dev@gmail.com>
2021-03-20 04:11:32 +0800

    day2021/3/20

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const _ = require('lodash')
+let tree = [
+  {
+    id: 1, name: 'name_1',
+    children: [
+      { id: 11, name: 'name_11' },
+      {
+        id: 12, name: 'name_12',
+        children: [
+          { id: 121, name: 'name_121' }
+        ]
+      }
+    ]
+  },
+  { id: 2, name: 'name_2', children: [{ id: 21, name: 'name_21' }] },
+  { id: 3, name: 'name_3' }
+]
 
-_.debounce()
+let newTree = JSON.parse(JSON.stringify(tree))
+
+let newTree = newTree.flat(3)
+
+function insert(m, n) {
+  return new Array(m).fill(n)
+}
